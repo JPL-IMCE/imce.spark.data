f05c8a62092a74fb7706ccb089ddf6251b158452
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-11-04 16:46:51 +0000

    Initial contribution

---
+++ build.sbt
@@ -0,0 +1,53 @@
+import sbt.Keys._
+import sbt._
+
+import gov.nasa.jpl.imce.sbt._
+import gov.nasa.jpl.imce.sbt.ProjectHelper._
+
+updateOptions := updateOptions.value.withCachedResolution(true)
+
+lazy val core = Project("imce-spark-data", file("."))
+  .enablePlugins(IMCEGitPlugin)
+  .enablePlugins(IMCEReleasePlugin)
+  .settings(IMCEPlugin.strictScalacFatalWarningsSettings)
+  .settings(
+    IMCEKeys.licenseYearOrRange := "2016",
+    IMCEKeys.organizationInfo := IMCEPlugin.Organizations.omf,
+
+    buildInfoPackage := "imce.spark.data",
+    buildInfoKeys ++= Seq[BuildInfoKey](BuildInfoKey.action("buildDateUTC") { buildUTCDate.value }),
+
+    projectID := {
+      val previous = projectID.value
+      previous.extra(
+        "build.date.utc" -> buildUTCDate.value,
+        "artifact.kind" -> "generic.library")
+    },
+
+    IMCEKeys.targetJDK := IMCEKeys.jdk18.value,
+    git.baseVersion := Versions.version,
+    // include all test artifacts
+    publishArtifact in Test := true,
+
+    scalaSource in Test := baseDirectory.value / "test",
+
+    libraryDependencies ++= Seq (
+      "org.apache.spark" %% "spark-core" % "2.0.1",
+      "org.apache.spark" %% "spark-sql" % "2.0.1"
+    ),
+
+    extractArchives := {},
+
+    resolvers += Resolver.bintrayRepo("jpl-imce", "gov.nasa.jpl.imce"),
+    resolvers += Resolver.bintrayRepo("tiwg", "org.omg.tiwg")
+  )
+  .dependsOnSourceProjectOrLibraryArtifacts(
+    "jpl-omf-schema-tables",
+    "jpl.omf.schema.tables",
+    Seq(
+      "gov.nasa.jpl.imce" %% "jpl-omf-schema-tables"
+        % Versions_omf_schema_tables.version
+    )
+  )
+  .dependsOn(ProjectRef(uri("file:/opt/local/imce/users/nfr/github.schema/jpl.omf.schema.tables"), "tablesJVM"))
+
---
+++ src/main/scala/gov/nasa/jpl/imce/spark/data/Example1.scala
@@ -0,0 +1,166 @@
+package gov.nasa.jpl.imce.spark.data
+
+import java.lang.System
+
+import org.apache.spark.SparkConf
+
+import scala.collection.JavaConversions._
+import scala.util.control.Exception._
+import java.io.InputStream
+
+import gov.nasa.jpl.imce.omf.schema._
+import gov.nasa.jpl.imce.omf.schema.tables.{IRI,LocalName,UUID}
+import org.apache.commons.compress.archivers.zip.{ZipArchiveEntry, ZipFile}
+import org.apache.spark.sql.{Dataset, SparkSession}
+
+import scala.collection.immutable._
+import scala.{Array,StringContext,Unit}
+import scala.Predef.String
+
+import scala.io
+
+object Example1 {
+
+  case class TerminologyGraphData( uuid: UUID, kind: String, name: LocalName, iri: IRI)
+
+  object TerminologyGraphData {
+
+    implicit def toTerminologyGraphData
+    (tg: tables.TerminologyGraph)
+    : TerminologyGraphData
+    = TerminologyGraphData(tg.uuid, tg.kind.toString, tg.name, tg.iri)
+
+    implicit def toTerminologyGraph
+    (tgd: TerminologyGraphData)
+    : tables.TerminologyGraph
+    = tables.TerminologyGraph(tgd.uuid, tables.TerminologyGraphKind.fromString(tgd.kind), tgd.name, tgd.iri)
+
+  }
+
+  def readJSonTable[T](is: InputStream, fromJSon: String => T)
+  : Seq[T]
+  = io.Source.fromInputStream(is).getLines.map(fromJSon).to[Seq]
+
+  case class OMFTables
+  (graphs: Seq[tables.TerminologyGraph] = Seq.empty,
+   concepts: Seq[tables.Concept] = Seq.empty,
+   conceptSpecializations: Seq[tables.ConceptSpecializationAxiom] = Seq.empty,
+   binaryScalarRestrictions: Seq[tables.BinaryScalarRestrictionAxiom] = Seq.empty) {
+
+    def readGraphs(is: InputStream)
+    : OMFTables
+    = copy(graphs = readJSonTable(is, tables.TerminologyGraphHelper.fromJSON))
+
+    def readConcepts(is: InputStream)
+    : OMFTables
+    = copy(concepts = readJSonTable(is, tables.ConceptHelper.fromJSON))
+
+    def readConceptSpecializations(is: InputStream)
+    : OMFTables
+    = copy(conceptSpecializations = readJSonTable(is, tables.ConceptSpecializationAxiomHelper.fromJSON))
+
+    def readBinaryScalarRestrictions(is: InputStream)
+    : OMFTables
+    = copy(binaryScalarRestrictions = readJSonTable(is, tables.BinaryScalarRestrictionAxiomHelper.fromJSON))
+
+    override def toString: String =
+    "OMFTables{"+
+    graphs.map(tables.TerminologyGraphHelper.toJSON).mkString("\ngraphs:{\n",",\n","\n}")+
+    concepts.map(tables.ConceptHelper.toJSON).mkString("\nconcepts:{\n",",\n","\n}")+
+    conceptSpecializations.map(tables.ConceptSpecializationAxiomHelper.toJSON).mkString("\nconceptSpecializations:{\n",",\n","\n}")+
+    "}"
+
+    def toDS
+    ()
+    (implicit spark: SparkSession)
+    : OMFDataSet
+    = {
+      import spark.implicits._
+      OMFDataSet(
+        graphs=graphs.map(TerminologyGraphData.toTerminologyGraphData).toDS(),
+        concepts=concepts.toDS(),
+        conceptSpecializations=conceptSpecializations.toDS(),
+        binaryScalarRestrictions=binaryScalarRestrictions.toDS())
+    }
+  }
+
+  def mergeTables
+  (t1: OMFTables, t2: OMFTables)
+  : OMFTables
+  = OMFTables(
+    graphs = t1.graphs ++ t2.graphs,
+    concepts = t1.concepts ++ t2.concepts,
+    conceptSpecializations = t1.conceptSpecializations ++ t2.conceptSpecializations,
+    binaryScalarRestrictions = t1.binaryScalarRestrictions ++ t2.binaryScalarRestrictions)
+
+  def readZipArchive
+  (zipFile: ZipFile)
+  (tables: OMFTables, ze: ZipArchiveEntry)
+  : OMFTables
+  = {
+    val is = zipFile.getInputStream(ze)
+    ze.getName match {
+      case "TerminologyGraphs.json" =>
+        tables.readGraphs(is)
+      case "Concepts.json" =>
+        tables.readConcepts(is)
+      case "ConceptSpecializationAxioms.json" =>
+        tables.readConceptSpecializations(is)
+    }
+  }
+
+  case class OMFDataSet
+  (graphs: Dataset[TerminologyGraphData],
+   concepts: Dataset[tables.Concept],
+   conceptSpecializations: Dataset[tables.ConceptSpecializationAxiom],
+   binaryScalarRestrictions: Dataset[tables.BinaryScalarRestrictionAxiom])
+
+  /**
+    * @param args The 1st argument must be an absolute path to a ZIP file of OMF Schema tables.
+    */
+  def main(args: Array[String]): Unit = {
+
+    if (args.size != 1) {
+      System.err.println(s"Usage: ${this.getClass.getSimpleName} <OMF Schema table ZIP file>")
+      System.exit(-1)
+    }
+
+    val conf = new SparkConf().setMaster("local").setAppName(this.getClass.getSimpleName)
+
+    implicit val spark = SparkSession
+      .builder()
+      .config(conf)
+      .getOrCreate()
+
+    try {
+      nonFatalCatch[Unit]
+        .withApply {
+          (cause: java.lang.Throwable) =>
+            System.out.println(s"Error: $cause")
+            cause.printStackTrace()
+        }
+        .apply {
+          val path = args(0)
+          val zipFile = new ZipFile(path)
+          val tables =
+            zipFile
+              .getEntries
+              .toIterable
+              .par
+              .aggregate(OMFTables())(seqop = readZipArchive(zipFile), combop = mergeTables)
+          zipFile.close()
+
+          System.out.println(s"tables: ")
+          System.out.println(tables)
+
+          val dtables = tables.toDS()
+          System.out.println(s"\n as DS: ")
+          System.out.println(dtables.concepts)
+
+        }
+    } finally {
+      spark.stop()
+    }
+  }
+
+}
